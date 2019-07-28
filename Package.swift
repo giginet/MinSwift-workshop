3378e537fb0f810a3e6283d8d891aeb11e51cb37
omochimetaru <omochi.metaru@gmail.com>
2019-07-28 17:33:36 +0900

    LLVM8

--- Package.swift
+++ Package.swift
@@ -2,8 +2,6 @@
 
 import PackageDescription
 
-let rpath = "/Applications/Xcode-beta.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift/macosx"
-
 let package = Package(
     name: "MinSwift",
     platforms: [.macOS(.v10_14)],
@@ -17,18 +15,16 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-syntax.git",
                  .revision("swift-DEVELOPMENT-SNAPSHOT-2019-07-10-m")),
         .package(url: "https://github.com/llvm-swift/LLVMSwift.git",
-                 .revision("0.4.1")),
+                 .upToNextMinor(from: "0.6.0")),
+        
+        // LLVMSwiftと同じ記述にする
         .package(url: "https://github.com/llvm-swift/FileCheck.git",
                  from: "0.0.3")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(name: "minswift", dependencies: ["MinSwiftKit"],
-                linkerSettings: [
-                    .unsafeFlags(["-rpath", rpath])
-            ]
-        ),
+        .target(name: "minswift", dependencies: ["MinSwiftKit"]),
         .target(
             name: "MinSwiftKit",
             dependencies: ["SwiftSyntax", "LLVM"]),
